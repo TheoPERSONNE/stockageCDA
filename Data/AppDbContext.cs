e2be517f8c888446bed1c9627995506795070464
theo.personne <theo.personne@live.fr>
2024-05-02 10:29:40 +0200

    Add files via upload
    data

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using APPCDA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APPCDA.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Article> Article { get; set; }
+        public DbSet<Ticket> Ticket { get; set; }
+        public DbSet<Commande> Commande { get; set; }
+        public DbSet<ContactUs> ContactUs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+            modelBuilder.Entity<Commande>()
+                .HasKey(c => c.IdCommande);
+
+            // Add other configurations as needed...
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
